9fb70c48f32463e52335af3885e80954be4b0804
jwons <joe.wonsil@gmail.com>
2018-06-13 09:59:54 -0400

    Added ddg and parser

--- R/debug.R
+++ R/debug.R
@@ -1,4 +1,18 @@
+library(RDataTracker)
+library(provParseR)
+library(provGraphR)
 
 debug.init <- function(input.data) {
+  file.parts <- strsplit(input.data, "\\.")
+  file.ext <- file.parts[[1]][[length(file.parts[[1]])]]
+
+  if (file.ext == "R" || file.ext == "Rmd") {
+    ddg.run(input.data)
+    prov.parse(ddg.json(), isFile = F)
+  } else if (file.ext == "json") {
+    prov.parse(input.data)
+  } else {
+    warning("Please enter a valid script or prov.json name")
+  }
 
 }
