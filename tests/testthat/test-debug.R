2dfabd1983ccc3fb9c3e140e6481d2d79d876f01
Orenna Brand <obrand@commschool.org>
2018-06-13 10:29:40 -0400

    added tests

--- tests/testthat/test-debug.R
+++ tests/testthat/test-debug.R
@@ -1,5 +1,19 @@
+library(RDataTracker)
+library(provParseR)
+library(provGraphR)
+library(provDebugR)
+
 context("test-debug.R")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+# Loading test data
+test.data <- system.file("testdata", "test.json", package = "provDebugR")
+debug.init(test.data)
+
+context("if input is R or Rmd")
+# R script not working
+
+context("if input is json")
+envi.df <- get.environment
+
+#string split ->
+#call prov.parse access functions
