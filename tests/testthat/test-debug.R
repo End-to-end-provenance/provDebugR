76a1282db3079ada1974d1027ac8596c5612797b
Orenna Brand <obrand@commschool.org>
2018-06-13 11:00:40 -0400

    added fromLine file

--- tests/testthat/test-debug.R
+++ tests/testthat/test-debug.R
@@ -7,10 +7,15 @@ library(provDebugR)
 test.data <- system.file("testdata", "test.json", package = "provDebugR")
 debug.init(test.data)
 
-# context("If input is R or Rmd")
+# context("R or Rmd input")
 # R script not working
 
-context("If input is json")
+context("JSON input")
 envi.df <- get.environment()
 expect_match(class(envi.df), "data.frame")
 expect_match(typeof(envi.df$value), "character")
+
+context("Graph")
+s <- get.spine("p11")
+expect_match(class(s), "character")
+expect_equal(length(s), 2)
