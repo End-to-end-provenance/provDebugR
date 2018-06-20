a0062bf7e38312adfb8d6858930b038f8b17703d
jwons <joe.wonsil@gmail.com>
2018-06-20 13:39:02 -0400

    Added tests and debug error now returns a data frame as opposed to a list of a single data frame

--- R/debugIssues.R
+++ R/debugIssues.R
@@ -3,39 +3,39 @@ debug.error.trace <- function(stack.overflow = F) {
   if(!debug.env$has.graph) {
     stop("debug.init must be run first")
   }
-  
+
   if(stack.overflow) {
     warning("stack overflow functionality is currently not supported")
   }
-  debug.lineage("error.msg")
+  debug.lineage("error.msg")$error.msg
 }
 
 # This function operates similarily to debug.lineage; however,
-# it uses just the warning messages. The whole debug.lineage function 
+# it uses just the warning messages. The whole debug.lineage function
 # cannot be reused here as it relies on variables having unique
 # names. warnings do not have unique names and there can be multiple
-# warnings unlike error messages. 
+# warnings unlike error messages.
 debug.warning.trace <- function(..., stack.overflow = F) {
   args <- list(...)
-  
+
   # This function is useless unless the adj.graph exists
   if(!debug.env$has.graph) {
     stop("debug.init must be run first")
   }
-  
-  # Grab all the warning rows from the provenance 
+
+  # Grab all the warning rows from the provenance
   pos.vars <- get.data.nodes()
   pos.vars <- pos.vars[pos.vars$name == "warning.msg", ]
   row.names(pos.vars) <- 1:nrow(pos.vars)
-  
+
   node.labels <- as.list(pos.vars$label)
-  
+
   # Extract the warning messages to display to the user
   # as options, the length will help determine whether or
   # not a valid result was input as an arg
   pos.results <- as.list(pos.vars$value)
   num.results <- 1:length(pos.results)
-  
+
   # Checks each arg to make sure it is valid
   # producing a vector of logicals corresponding with
   # valid/invalid input
@@ -47,18 +47,18 @@ debug.warning.trace <- function(..., stack.overflow = F) {
       return(FALSE)
     }
   })
-  
+
   # Any non-valid inputs will be removed as the list is subset
   # by logicals, TRUE corresponding to valid inputs
   args <- args[unlist(pos.args)]
-  
+
   if(length(args) == 0) {
     cat("Possible results: \n")
     print(pos.vars$value)
     cat("Pass the corresponding index value to the function for info on that warning")
   } else {
     proc.nodes <- get.proc.nodes()
-    
+
     dfs <- lapply(args, function(arg){
       .process.label(pos.vars[arg, ]$label, proc.nodes, forward = F)
     })
--- tests/testthat/test-debug.R
+++ tests/testthat/test-debug.R
@@ -18,6 +18,8 @@ expect_match(typeof(envi.df$value), "character")
 
 context("Graph")
 s <- get.spine("p11")
+expect_equal(length(s), 5)
+expect_match(s[3], "p6")
 
 context("Debugging Lineage")
 dl.df <- debug.lineage("x")
@@ -25,4 +27,12 @@ expect_match(dl.df$x$code[1], "y <- 2")
 expect_equal(dl.df$x$line[1], 3)
 dl.df <- debug.lineage("y", forward = T)
 expect_match(dl.df$y$code[1], "y <- 2")
-expect_match(dl.df$y$code[6], "x <- b + y")
+expect_match(dl.df$y$code[6], "x <- b \\+ y")
+dl.dfs <- debug.lineage("x", "y")
+expect_equal(length(dl.dfs), 2)
+
+context("Debug Warnings and Errors")
+debug.error <- debug.error.trace()
+
+debug.warn <- debug.warning.trace(1,2)
+
