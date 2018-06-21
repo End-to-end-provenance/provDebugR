05fabe89657d341efb971abd6f7e3ef9243eb795
jwons <joe.wonsil@gmail.com>
2018-06-21 16:02:07 -0400

    allowed araguments of debug.variable.type to be lists or single elements

--- R/debugType.R
+++ R/debugType.R
@@ -1,18 +1,26 @@
 debug.variable.type <- function(..., just.logical = F) {
   # Collect the arguments passed to the function
   args <- list(...)
-  
+
+  flat.args <- list()
+
+  lapply(args, function(arg){
+    flat.args <<- append(flat.args, arg)
+  })
+
+  args <- flat.args
+
   # This function is useless unless the adj.graph exists
   if(!debug.env$has.graph) {
     stop("debug.init must be run first")
   }
-  
+
   # Collect possible results the user could ask for
   # Remove any rows that aren't variables
   pos.vars <- get.data.nodes()
   pos.vars <- pos.vars[pos.vars$type == "Data" | pos.vars$type == "Snapshot", ]
   pos.vars <- as.list(unique(pos.vars$name))
-  
+
   # Make sure all the results passed by the user are valid
   # this produces a list of logicals, where TRUES
   #correspond to valid inputs
@@ -24,15 +32,15 @@ debug.variable.type <- function(..., just.logical = F) {
       return(FALSE)
     }
   })
-  
+
   # Any non-valid inputs will be removed as the list is subset
   # by logicals, TRUE corresponding to valid inputs
   args <- args[unlist(pos.args)]
-  
+
   # If they did not provide any results themselve, list them out for them
   if(length(args) == 0) {
     cat("Possible results:\n")
-    print(unlist(pos.vars))
+    return(unlist(pos.vars))
   } else {
     ret.val <- lapply(args, .grab.instances)
     names(ret.val) <- args
@@ -45,28 +53,39 @@ debug.variable.type <- function(..., just.logical = F) {
   data.nodes <- get.data.nodes()
   proc.nodes <- get.proc.nodes()
   proc.data <- get.proc.data()
-  
+
+  # These data node are the rows that math with the provided result
   res.dat.nodes <- data.nodes[data.nodes$name == result, ]
+
+  # These procedure labels are those that have connections with the chosen result nodes
   res.proc.labels <- proc.data[proc.data$entity %in% res.dat.nodes$label, ]$activity
-  
+
+  # These procedure nodes are the ones that have connections with the chosen result nodes
   res.proc.rows <- proc.nodes[proc.nodes$label %in% res.proc.labels, ]
-  
+
+  # Now that the data and procedure nodes have been narrowed down to only the
+  # requested information, the information that will be provided to the user
+  # is grabbed
   script <- res.proc.rows$scriptNum
   line <- res.proc.rows$startLine
   scope <- res.dat.nodes$scope
-  
+
+  # Since the type is stored as a json in the data frame, the type needs to be extraced
+  # If it is a data frame, inform the user of its dimensions
   type <- unlist(lapply(res.dat.nodes$valType, function(type){
     valType <- jsonlite::fromJSON(type)
     if(valType$container == "data_frame") {
-      return(paste(valType$container, ", with dimensions: ", valType$dimension[1], ",", valType$dimension[2], sep=""))
+      return(paste(valType$container,
+                   ", with dimensions: ",
+                   valType$dimension[1],
+                   ",",
+                   valType$dimension[2],
+                   sep=""))
     } else {
       return(valType$type)
     }
     }))
-  
-  df <- as.data.frame(cbind(script, line, scope, type), stringsAsFactors = F)
-}
 
-debug.from.type <- function(variable, type) {
-  cat("Not yet implemented")
+  # Combine all the data and return to the user
+  df <- as.data.frame(cbind(script, line, scope, type), stringsAsFactors = F)
 }
