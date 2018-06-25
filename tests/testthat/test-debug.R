7b730eaa3bde14d6d5ade1df684b09a99364c078
jwons <joe.wonsil@gmail.com>
2018-06-25 10:01:01 -0400

    added variable type tests

--- tests/testthat/test-debug.R
+++ tests/testthat/test-debug.R
@@ -5,6 +5,7 @@ library(provDebugR)
 library(igraph)
 
 # Loading test data
+context("Initialize Debugger")
 test.data <- system.file("testdata", "test.json", package = "provDebugR")
 debug.prov(test.data)
 
@@ -27,7 +28,7 @@ expect_match(dl.df$x$code[1], "y <- 2")
 expect_equal(dl.df$x$line[1], 3)
 dl.df <- debug.lineage("y", forward = T)
 expect_match(dl.df$y$code[1], "y <- 2")
-expect_match(dl.df$y$code[6], "x <- b \\+ y")
+expect_match(dl.df$y$code[6], "x <- b + y", fixed = T)
 dl.dfs <- debug.lineage("x", "y")
 expect_equal(length(dl.dfs), 2)
 
@@ -38,4 +39,15 @@ expect_equal(debug.error$line[2], 24)
 
 debug.warn <- debug.warning.trace(1,2)
 expect_equal(debug.warn[[2]]$line, 22)
+expect_equal(length(debug.warn), 2)
 
+context("Debug Variable Type")
+var.type.results <- debug.variable.type()
+expect_equal(length(var.type.results), 9)
+expect_match(var.type.results[3], "y")
+var.type <- debug.variable.type(var.type.results)
+expect_equal(length(var.type), length(var.type.results))
+expect_match(var.type[[1]]$line[1], "1")
+expect_match(var.type[[1]]$line[2], "10")
+
+context("Debug ")
