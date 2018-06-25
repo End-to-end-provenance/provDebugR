9575edc5afc84cc45e52991ac04fc6008bcb25cf
Orenna Brand <o.brand@columbia.edu>
2018-06-25 15:14:30 -0400

    implemented debug.from.type

--- R/debugFromType.R
+++ R/debugFromType.R
@@ -18,12 +18,37 @@
 debug.from.type <- function(var, type) {
   # if either parameter is left blank, return an error message
 
-  if (length(var) <= 0) {
-    warning("Please enter variable")
+  data.nodes <- get.data.nodes()
+  proc.nodes <- get.proc.nodes()
+  proc.data <- get.proc.data()
+
+  labels <- data.nodes[data.nodes$name == var, "label"]
+  var.types <- data.nodes[data.nodes$name == var, "valType"]
+  var.types <- lapply(var.types, jsonlite::fromJSON)
+
+  is.type.match <- function(var.type) {
+    var.type <- unlist(var.type)
+    if (var.type["type"] == type) {
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
   }
 
-  if (length(type) <= 0) {
-    warning("Please enter type")
-  }
+  type.logicals <- unlist(lapply(var.types, is.type.match))
+  labels <- labels[type.logicals]
 
+  label.rows <- lapply(labels, function(label) {
+    # Get line number and code from corresponding procedure node
+    proc.node <- proc.data[proc.data$entity == label, "activity"]
+    line <- proc.nodes[proc.nodes$label == proc.node, "startLine"]
+    name <- proc.nodes[proc.nodes$label == proc.node, "name"]
+
+    label.row <- c(line, name)
+  })
+
+  label.df <- as.data.frame(do.call(rbind, label.rows), stringsAsFactors = F)
+  colnames(label.df) <- c("line", "code")
+
+  return(label.df)
 }
