69328c7ad374dc9faa739e51f267ced26a845871
Orenna Brand <o.brand@columbia.edu>
2018-06-27 09:29:19 -0400

    added multiline statement

--- inst/testdata/testScript.R
+++ inst/testdata/testScript.R
@@ -2,10 +2,10 @@ x <- 1
 a <- 5
 y <- 2
 z <- 3 + y
-c <- 6 
+c <- 6
 
 i <- 7 + y
-j <- i 
+j <- i
 
 x <- 5 + z
 
@@ -17,7 +17,10 @@ j <- list("another test", 5L, 6.5)
 
 df <-  data.frame(1:500, 501:1000, 1001:1500)
 names(df) <- c("foo", "bar", "foobar")
-df1 <- data.frame(c(0,0,0,0), c(1,3,5,8), c("x <- 1", "y <- 2", "z <- 3", "cor(c(x, x), c(y, z))"), stringsAsFactors = F)
+df1 <- data.frame(c(0,0,0,0), c(1,3,5,8),
+                  c("x <- 1", "y <- 2", "z <- 3", "cor(c(x, x), c(y, z))"),
+                  stringsAsFactors = F)
+
 names(df1) <- c("script", "line", "code")
 
 warning.trace <- function(x, forward = F) {return(df1)}
