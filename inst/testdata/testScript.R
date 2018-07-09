b41138434c878848b7f4edaa512d0539e773d485
Orenna Brand <o.brand@columbia.edu>
2018-07-09 09:31:47 -0400

    worked on shiny stuff

--- inst/testdata/testScript.R
+++ inst/testdata/testScript.R
@@ -15,16 +15,16 @@ z <- 6:67
 
 j <- list("another test", 5L, 6.5)
 
-df <-  data.frame(1:500, 501:1000, 1001:1500)
+df <- data.frame(1:500, 501:1000, 1001:1500)
 names(df) <- c("foo", "bar", "foobar")
-df1 <- data.frame(c(0,0,0,0),
-                  c(1,3,5,8),
-                  c("x <- 1", "y <- 2", "z <- 3", "cor(c(x, x), c(y, z))"),
-                  stringsAsFactors = F)
+df1 <- data.frame(c(0, 0, 0, 0), c(1, 3, 5, 8), c("x <- 1", "y <- 2",
+  "z <- 3", "cor(c(x, x), c(y, z))"), stringsAsFactors = F)
 
 names(df1) <- c("script", "line", "code")
 
-warning.trace <- function(x, forward = F) {return(df1)}
+warning.trace <- function(x, forward = F) {
+  return(df1)
+}
 
 source("./inst/testdata/test3.R")
 
