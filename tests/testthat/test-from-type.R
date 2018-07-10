f5e8fc2f27494910c0a89e9f1a3af65762e0c1ba
oebrand <oeb2107@columbia.edu>
2018-07-10 09:40:47 -0400

    added tests for debug.from.type

--- tests/testthat/test-from-type.R
+++ tests/testthat/test-from-type.R
@@ -4,3 +4,13 @@ library(provDebugR)
 
 test.data <- system.file("testdata", "test.json", package = "provDebugR")
 debug.init(test.data)
+
+test_that("the correct types are being returned", {
+  type.results <- debug.from.type("x", "numeric")
+  expect_match(class(type.results), "data.frame")
+})
+
+test_that("wrong arguments can be ignored", {
+  type.results <- debug.from.type("y", "numeric")
+  expect_match(class(type.results), "character")
+})
