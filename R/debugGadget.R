29de0b777ae83d5a5179d5414db0291100e73544
oebrand <oeb2107@columbia.edu>
2018-07-12 13:32:35 -0400

    saving changes

--- R/debugGadget.R
+++ R/debugGadget.R
@@ -4,11 +4,49 @@ library(provDebugR)
 
 debugGadget <- function() {
   
-  ui <- ...
+  ui <- miniPage(
+    gadgetTitleBar("provDebugR"),
+    miniContentPanel(padding = 15,
+      fileInput(inputId = "file",
+                label = "Choose R script or prov-JSON file",
+                accept = c(
+                  ".R",
+                  ".Rmd",
+                  ".json"),
+                multiple = FALSE,
+                buttonLabel = "Browse...",
+                placeholder = "No file selected"
+                ),
+      fillRow(textInput(inputId = "lines",
+                        label = "Lines", #NULL?
+                        value = "",
+                        placeholder = "Enter the lines you want to examine")
+              ),
+      #hr(),
+      fillRow(verbatimTextOutput("value")),
+      tableOutput("return")
+      )
+  )
   
+  #session?
   server <- function(input, output) {
-    ...
+    
+    #debug.init(input$file)
+    
+    output$lines <- renderPrint({
+      debug.from.line(input$lines)
+    })
+    
+    output$value <- renderPrint({
+      input$file#,
+      #env = .debug.env()
+    })
+    
+    output$return <- renderTable({
+      #debug.from.line()
+    })
   }
   
-  runGadget(ui, server, viewer = dialogViewer("provDebugR"))
+  # dialogViewer("provDebugR")?
+  runGadget(ui, server, viewer = paneViewer())
 }
