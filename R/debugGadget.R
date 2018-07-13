e97e5ac040b3a7653b2fb7ed82de228fd06c039b
oebrand <oeb2107@columbia.edu>
2018-07-13 16:24:44 -0400

    changed name of function

--- R/debugGadget.R
+++ R/debugGadget.R
@@ -1,10 +1,10 @@
-debugGadget <- function() {
+debug.gadget <- function() {
 
   ui <- miniPage(
     gadgetTitleBar("provDebugR"),
     miniContentPanel(
       fillRow(
-        flex = c(1, 3),
+        flex = c(1, 2),
         fillCol(
           fileInput(inputId = "file",
                     label = "Choose R script or prov-JSON file",
@@ -20,13 +20,16 @@ debugGadget <- function() {
                                       "Reference" = FALSE),
                        selected = TRUE),
           textInput(inputId = "lines",
-                    label = "Enter lines to examine, separated by a comma or colon (for range)",
+                    label = "Enter lines to examine, separated by a comma",
                     value = "",
                     placeholder = "E.g., 1, 3:5, 10")
           ),
         fillCol(
+          #vervatimTextOutput(outputId = "code"),
+          tags$h4("Code"),
+          uiOutput("code", container = rCodeContainer),
+          tags$h4("Output"),
           verbatimTextOutput(outputId = "value")
-          #uiOutput("document", container = rCodeContainer)
         )
       )
     )
@@ -38,35 +41,36 @@ debugGadget <- function() {
     reactiveInit <- reactive({
       file <- input$file$datapath
       debug.init(file)
-    
-      # interesting - automatically tries to run on line 0
-      # environment issue?
     })
     
+    # reactiveDebug <- observeEvent(input$lines, {
     reactiveDebug <- reactive({
       args <- unname(as.numeric(unlist(strsplit(input$lines, ","))))
       state <- as.logical(input$state)
-      
       debug.from.line(args, state = state, script = 0)
     })
 
+    output$code <- renderUI({
+      input$file$name
+      #name <- get.proc.nodes()[get.proc.nodes()$startLine == 2, "name"]
+      #name <- name[!is.na(name)]
+    })
+    
     output$value <- renderPrint({
       # put message for when file hasn't been inputed yet
-      # state
       reactiveInit()
       if (input$lines != "") {
         reactiveDebug()
       } else {
-        #debug.from.line()
         print("No lines entered")
+        #debug.from.line()
       }
     })
     
     observeEvent(input$done, {
-      #to return last lines queried:
+      #to return last lines queried: (might be an error if there's no line entered yet)
       returnValue <- reactiveDebug()
       stopApp(returnValue)
-      #stopApp()
     })
     
     observeEvent(input$cancel, {
@@ -77,3 +81,17 @@ debugGadget <- function() {
   # paneViewer()
   runGadget(ui, server, viewer = dialogViewer("provDebugR", width = 1000, height = 800))
 }
+
+rCodeContainer <- function(...) {
+  code <- HTML(as.character(tags$code(class = "language-r", ...)))
+  div(pre(code))
+}
+
+
+#ideas:
+# find scripts in scripts folder in ddg folder
+# make each line a button, click a line and it would tell you what's going on
+# action button, next/previous line -- returns info about what variables are referenced on each line
+# two panels, one with debug.from.line and the other with a step-through debugger
+#   not sure which is more useful
+# observeEvent or reactiveEvent which triggers debug.from.line()
