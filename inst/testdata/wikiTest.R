cc890bab88afae356d65578a515ffbeb71692a75
oebrand <oeb2107@columbia.edu>
2018-07-13 18:49:16 -0400

    got rid of whitespace

--- inst/testdata/wikiTest.R
+++ inst/testdata/wikiTest.R
@@ -1,13 +1,9 @@
 x <- data.frame(1:5, 6:10)
 y <- 2
 z <- 3 + y
-
 y <- "a test"
-
 j <- list("another test", 5L, 6.5)
-
 warning.trace <- function(x, forward = F) {
   return(df1)
 }
-
 source("./inst/testdata/test3.R")
