ff903ebe4998ce2b546ca679bdae2225a21c24db
oebrand <oeb2107@columbia.edu>
2018-07-13 16:40:41 -0400

    made short test script for wiki

---
+++ inst/testdata/wikiTest.R
@@ -0,0 +1,13 @@
+x <- data.frame(1:5, 6:10)
+y <- 2
+z <- 3 + y
+
+y <- "a test"
+
+j <- list("another test", 5L, 6.5)
+
+warning.trace <- function(x, forward = F) {
+  return(df1)
+}
+
+source("./inst/testdata/test3.R")
