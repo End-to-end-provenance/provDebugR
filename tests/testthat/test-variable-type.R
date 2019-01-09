197f26cfe0db72bad4959aca997e2daf164ba9ed
Barbara Lerner <blerner@mtholyoke.edu>
2019-01-09 15:33:25 -0500

    debug.variable.type provides more meaningful output

--- tests/testthat/test-variable-type.R
+++ tests/testthat/test-variable-type.R
@@ -23,8 +23,6 @@ test_that("possible results can be grabbed", {
 
 test_that("all variables can be queried", {
   variable.results <- debug.variable.type(debug.variable.type())
-  print ("variable.results")
-  print (variable.results)
   expect_equal(length(variable.results), 12)
   expect_equal(variable.results$x$line[[1]], "1")
   expect_match(variable.results$y$type[[1]], "numeric")
@@ -46,6 +44,13 @@ test_that("wrong arguments can be ignored", {
   expect_equal(length(variable.results), 12)
 })
 
+test_that("just.logical works", {
+      variable.results <- debug.variable.type(debug.variable.type(), just.logical=TRUE)
+      expect_equal (variable.results$x, "Type did not change")
+      expect_equal (variable.results$y, "Type changed")
+    })
+
+
 test.data <- system.file("testdata", "specialChar.json", package = "provDebugR")
 debug.init(test.data)
 
@@ -55,5 +60,4 @@ test_that("special values are being grabbed", {
   expect_match(variable.results$y$type[3], "environment")
   #expect_match(variable.results$y$type[2], "language")  #**NOT currently supported by RDT
 })
-
 
