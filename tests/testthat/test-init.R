ee9e9406f3353382adefca93005e028c476cc747
fong22e <fong22e@mtholyoke.edu>
2019-06-18 16:10:17 -0400

    updated test-init for empty case

--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -2,10 +2,14 @@ context("Intialization")
 
 library(provDebugR)
 
-test.data <- system.file("testdata", "test.json", package = "provDebugR")
-debug.init(test.data)
-
-test_that("the debugger can be initialized", {
-  expect_true(provDebugR:::.debug.env$has.graph)
+test_that("Debugger initialization on empty provenance", {
+  test.data <- system.file("testdata/cases", "empty.json", package = "provDebugR")
+  expect_error(debug.init(test.data))
+  expect_false(provDebugR:::.debug.env$has.graph)
 })
 
+test_that("the debugger can be initialized", {
+  test.data <- system.file("testdata", "test.json", package = "provDebugR")
+  debug.init(test.data)
+  expect_true(provDebugR:::.debug.env$has.graph)
+})
