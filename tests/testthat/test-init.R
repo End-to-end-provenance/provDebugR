c4fb13c90f48a226b1a3f461be4de6d0d9d2a797
fong22e <fong22e@mtholyoke.edu>
2019-06-18 16:13:01 -0400

    updated test-init with correct file paths

--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -3,7 +3,7 @@ context("Intialization")
 library(provDebugR)
 
 test_that("Debugger initialization on empty provenance", {
-  test.data <- system.file("testdata/cases", "empty.json", package = "provDebugR")
+  test.data <- system.file("testdata", "empty.json", package = "provDebugR")
   expect_error(debug.init(test.data))
   expect_false(provDebugR:::.debug.env$has.graph)
 })
