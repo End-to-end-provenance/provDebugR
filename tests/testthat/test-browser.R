62a63ca67e223280694f135f047175fb5794a5d4
fong22e <fong22e@mtholyoke.edu>
2019-06-18 16:15:33 -0400

    updated test-browser with empty case

---
+++ tests/testthat/test-browser.R
@@ -0,0 +1,18 @@
+# author: Elizabeth Fong
+library(testthat)
+library(provDebugR)
+library(provParseR)
+
+context("Browser")
+
+# no provenance
+test_that("no/empty provenance", {
+	
+	# debugger has not been initialised
+	expect_error(debug.browser())
+	
+	# no provenance (no proc nodes)
+	json <- system.file("testdata", "empty.json", package = "provDebugR")
+	expect_error(debug.init(json))
+	expect_error(debug.browser())
+})
