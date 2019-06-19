0da2d47aab21a07c2f00948fdfc92588d1afe6d4
fong22e <fong22e@mtholyoke.edu>
2019-06-19 12:06:19 -0400

    add test for step-in tables

--- tests/testthat/test-browser.R
+++ tests/testthat/test-browser.R
@@ -17,12 +17,25 @@ test_that("no/empty provenance", {
 	expect_error(debug.browser())
 })
 
+# step-in table
 test_that("step-in table", {
 	
-	# no step-in locations
-	json <- system.file("testdata", "stepin0.json", package = "provDebugR")
-	proc.nodes <- get.proc.nodes(prov.parse(json))
+	# cases
+	# 0, 1, 3 step-in locations
+	c0 <- system.file("testdata", "stepin0.json", package = "provDebugR")
+	#c1 <- system.file("testdata", "stepin1.json", package = "provDebugR")
+	#c3 <- system.file("testdata", "stepin3.json", package = "provDebugR")
+	
+	t0 <- .get.step.in.table(get.proc.nodes(prov.parse(c0)))
+	#t1 <- .get.step.in.table(get.proc.nodes(prov.parse(c1)))
+	#t3 <- .get.step.in.table(get.proc.nodes(prov.parse(c3)))
+	
+	# expected
+	#e1 <- read.csv(system.file("testexpected", "stepin1.csv", package = "provDebugR"))
+	#e3 <- read.csv(system.file("testexpected", "stepin3.csv", package = "provDebugR"))
+	
+	# test
 	expect_true(nrow(.get.step.in.table(proc.nodes)) == 0)
-	
-	
+	#expect_equivalent(t1, e1)
+	e#xpect_equivalent(t3, e3)
 })
