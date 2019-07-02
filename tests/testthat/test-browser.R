35f683764232ac934f17c77315cd1f54b9e9e899
fong22e <fong22e@mtholyoke.edu>
2019-07-02 16:30:58 -0400

    added 1 test to forward movement

--- tests/testthat/test-browser.R
+++ tests/testthat/test-browser.R
@@ -26,9 +26,9 @@ test_that("step-in table", {
 	#c1 <- system.file("testdata", "stepin1.json", package = "provDebugR")
 	#c3 <- system.file("testdata", "stepin3.json", package = "provDebugR")
 	
-	t0 <- .get.step.in.table(get.proc.nodes(prov.parse(c0)))
-	#t1 <- .get.step.in.table(get.proc.nodes(prov.parse(c1)))
-	#t3 <- .get.step.in.table(get.proc.nodes(prov.parse(c3)))
+	t0 <- provDebugR:::.get.step.in.table(get.proc.nodes(prov.parse(c0)))
+	#t1 <- provDebugR:::.get.step.in.table(get.proc.nodes(prov.parse(c1)))
+	#t3 <- provDebugR:::.get.step.in.table(get.proc.nodes(prov.parse(c3)))
 	
 	# expected
 	#e1 <- read.csv(system.file("testexpected", "stepin1.csv", package = "provDebugR"))
@@ -39,3 +39,38 @@ test_that("step-in table", {
 	#expect_equivalent(t1, e1)
 	#expect_equivalent(t3, e3)
 })
+
+
+test.file <- system.file("testdata", "stepin3.json", package = "provDebugR")
+debug.init(test.file)
+
+prov <- provParseR::prov.parse(test.file)
+current.script <- 1
+
+proc.nodes <- provParseR::get.proc.nodes(provParseR::prov.parse(test.file))
+pos.lines <- stats::na.omit(proc.nodes$startLine)
+
+scripts <- provParseR::get.scripts(prov)
+
+var.env <- new.env(parent = emptyenv())
+var.env$call.stack <- list()
+var.env$lineIndex <- 1
+var.env$vars <- NA
+
+
+# params:
+# input, var.env, current.script, pos.lines, proc.nodes, scripts
+
+# move forward
+# TODO: also need to check returned table as well!
+test_that("forward movement", {
+	
+	returned <- NULL
+	
+	output <- capture.output(returned <- provDebugR:::.moveForward('n', var.env, current.script, pos.lines, proc.nodes, scripts))
+	expect_identical(output, "3: b <- 15")
+	
+	
+})
+
+# move backward
