91d482f094c2828fbc05d38a154fce69bb3f5d5f
fong22e <fong22e@mtholyoke.edu>
2019-07-19 17:19:16 -0400

    modified .load.variables

--- R/debugBrowser_new.R
+++ R/debugBrowser_new.R
@@ -30,11 +30,10 @@ debug.browser <- function()
 	
 	# create an environment to store all the variables loaded from Global
 	# load any pre-existing variables into vars.
-	# as they were not created due to the execution of the script, the script number is NA.
 	var.env$vars <- new.env(parent = var.env)
 	.load.variables(var.env$vars, 
 					.get.line.num(var.env$current.proc, var.env$proc.nodes),
-					NA)
+					var.env$current.script)
 	
 	# Print instructions on how to use the debugger as well as the name of the main script.
 	cat("Debugger initialized, type \"help\" for more information or Q to quit\n")
@@ -73,7 +72,138 @@ debug.browser <- function()
 {
 	data.nodes <- debug.from.line(line.num, state = T, script.num = script.num)[[1]]
 	
-	# TODO
+	# convert to data frame if resulting table is a matrix (all cells have the same type)
+	if(is.atomic(data.nodes))
+	{
+		# if it is a matrix of NA, there is nothing to load. return.
+		if(all(is.na(data.nodes)))
+			return()
+		
+		# there are variables to load. convert to data frame.
+		data.nodes <- as.data.frame(data.nodes)
+	}
+	
+	# load variables!
+	# the rest of this function is from the original verision of .load.variables
+	# the load process uses a temporary environment, which is later removed.
+	load.env <- new env()
+	
+	# Assign each variable and it's value to the created environment
+	apply(data.nodes, 1, function(row)
+	{
+		if(!is.na(row["val"][[1]]))
+		{
+			# Check if the value is a snapshot, regex here checks for starting with
+			# data and is a file with an extension
+			if(grepl("^data", row["val"][[1]]) & grepl("^.*\\.[^\\]+$", row["val"][[1]]))
+			{
+				# If the provenance folder was found the snapshots can be grabbed
+				# from the file system
+				if(!is.na(.debug.env$prov.folder))
+				{
+					# The file ext indicates what type of data will be stored and how to 
+					# read it back in for the user, the file name is also used to complete
+					# the path to the final file
+					file.parts <- strsplit(row["val"][[1]], "\\.")
+					file.ext <- tolower(file.parts[[1]][[length(file.parts[[1]])]])
+					file.name <- file.parts[[1]][1]
+
+					# A text file means that the data has been stored as an RObject
+					# this can be loaded back in simply using load()
+					if(file.ext == "txt") {
+						full.path <- paste(.debug.env$prov.folder,
+											"/", file.name,
+											".RObject", sep = "")
+						# Don't try and read in a file that could possibly not exist
+						if(file.exists(full.path)) {
+							var.name <- load(full.path, envir = load.env)
+							assign(row["var/code"][[1]], get(var.name, load.env), envir = vars)
+						} else {
+							assign(row["var/code"][[1]], "INCOMPLETE SNAPSHOT", envir = vars)
+						}
+					# csv could be matrix, array, or data_frame
+					} else if (file.ext == "csv") { 
+						# a data frame can be read in using the read.csv function
+						# which creates a data frame
+						if(row[["container"]] == "data_frame") {
+							full.path <- paste(.debug.env$prov.folder,
+												"/", file.name,
+												".csv", sep = "")
+							if(file.exists(full.path)) {
+								temp.var <- utils::read.csv(full.path, stringsAsFactors = F)
+								assign(row["var/code"][[1]], temp.var, envir = vars)
+							} else {
+								assign(row["var/code"][[1]], "INCOMPLETE SNAPSHOT", envir = vars)
+							}
+						# A vector can be read in using read.csv but then needs the vectors extracted
+						} else if (row[["container"]] == "vector" | row[["container"]] == "matrix") {
+							full.path <- paste(.debug.env$prov.folder,
+												"/", file.name,
+												".csv", sep = "")
+							# Grab each column out of the data frame and then bind to create matrix
+							# or just a single vector.
+							if(file.exists(full.path)) {
+								temp.var <- utils::read.csv(full.path, stringsAsFactors = F)
+								if(nrow(temp.var) == 0 ) {
+									if (row[["container"]] == "vector") {
+										temp.var <- rep("", length.out = as.integer(row[["dim"]]))
+									}
+								} else {
+									indexes <- 1:ncol(temp.var)
+									temp.var <- cbind(sapply(indexes, function(index) {
+													temp.var[[index]]
+												}))
+									# a single vector is formatted differently than a single column of a matrix
+									if(ncol(temp.var) == 1 & row[["container"]] == "vector" ) {
+										temp.var <- as.vector(temp.var)
+									}
+								}
+                  
+								assign(row["var/code"][[1]], temp.var, envir = vars)
+							
+							# No file found
+							} else {
+								assign(row["var/code"][[1]], "INCOMPLETE SNAPSHOT", envir = vars)
+							}
+							
+						# An array is very similar to a vector but can be coerced into an array post read
+						} else if (row[["container"]] == "array") {
+							if(file.exists(full.path)) {
+								temp.var <- read.csv(full.path, stringsAsFactors = F)
+								indexes <- 1:ncol(temp.var)
+								temp.var <- cbind(sapply(indexes, function(index) {
+												temp.var[[index]]
+											}))
+								temp.var <- as.array(temp.var)
+							} else {
+								assign(row["var/code"][[1]], "INCOMPLETE SNAPSHOT", envir = vars)
+							} 
+						
+						#No identifiable container
+						} else {
+							assign(row["var/code"][[1]], "INCOMPLETE SNAPSHOT", envir = vars)
+						} 
+					
+					# No identifiable file extension
+					} else {
+						assign(row["var/code"][[1]], "INCOMPLETE SNAPSHOT" , envir = vars)
+					}
+				
+				# If the prov.folder was not located
+				} else {
+					assign(row["var/code"][[1]], "SNAPSHOT/MISSING PROVENANCE" , envir = vars)
+				}
+			
+			#If the data is not a snapshot it can be loaded directly into the variable env
+			} else {
+				type <- jsonlite::fromJSON(row["type"])$type
+				assign(row["var/code"][[1]], methods::as(row["val"][[1]], type), envir = vars)
+			}
+		}
+	})
+
+	# remove the temporary loading environment. the loaded variables are in vars.
+	rm(list=ls(load.env), envir = load.env)
 }
 
 # for this current execution level, get the list of procedure nodes to step through
