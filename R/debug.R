b5891c291abcb94898ae693faef5db2075cd6568
fong22e <fong22e@mtholyoke.edu>
2019-08-27 11:15:30 -0400

    skeleton

---
+++ R/debug.R
@@ -0,0 +1,54 @@
+
+prov.debug <- function()
+{
+	
+}
+
+prov.debug.file <- function()
+{
+	
+}
+
+prov.debug.run <- function()
+{
+	
+}
+
+
+
+
+debug.error.trace <- function()
+{
+	
+}
+
+debug.line <- function()
+{
+	
+}
+
+debug.lineage <- function()
+{
+	
+}
+
+debug.state <- function()
+{
+	
+}
+
+debug.type.changes <- function()
+{
+	
+}
+
+debug.variable <- function()
+{
+	
+}
+
+debug.warning.trace <- function()
+{
+	
+}
+
