9006b6c300a1a83ec1ed82fde2c55de027ab5028
fong22e <fong22e@mtholyoke.edu>
2019-12-30 18:32:35 -0500

    errors warnings test case

---
+++ inst/testdata/prov_exceptions/scripts/exceptions.R
@@ -0,0 +1,6 @@
+col1 <- c(1:11)
+
+prov.source("source_warning.R")
+prov.source("source_error.R")
+
+print("exceptions test case complete")
---
+++ inst/testscripts/source_warning.R
@@ -0,0 +1,4 @@
+if(length(col1) > 10) {
+	col1 <- c(1:10)
+	warning("length was greater than 10")
+}
