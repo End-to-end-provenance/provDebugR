bf154d441072e22fa5a82d088466dfabb9d1d293
fong22e <fong22e@mtholyoke.edu>
2019-12-30 19:08:14 -0500

    test - .get.full.code

--- tests/testthat/test_init.R
+++ tests/testthat/test_init.R
@@ -17,3 +17,18 @@ test_that("Initialization - Empty provenance",
 })
 
 # .get.full.code
+test_that(".get.full.code",
+{
+	# uses test case for warnings and errors
+	# multiple scripts, some proc nodes span multiple lines
+	c0 <- system.file("testdata", "exceptions.json", package = "provDebugR")
+	
+	provDebugR::prov.debug.file(c0)
+	t0 <- provDebugR:::.get.full.code()
+	t0 <- data.frame(t0, stringsAsFactors = TRUE)
+	
+	e0 <- system.file("testexpected", "fullCode_exceptions.csv", package = "provDebugR")
+	e0 <- read.csv(e0, header = TRUE, row.names = 1, stringsAsFactors = TRUE)
+	
+	expect_equivalent(t0, e0)
+})
