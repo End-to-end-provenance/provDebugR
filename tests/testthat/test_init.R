941038645de4269fbf63499da90ee986912c70c7
fong22e <fong22e@mtholyoke.edu>
2019-12-31 02:03:02 -0500

    skeleton for other init tests

--- tests/testthat/test_init.R
+++ tests/testthat/test_init.R
@@ -3,10 +3,45 @@ library(testthat)
 context("Initialization")
 
 # prov.debug.file (general case)
+test_that("Initialization - prov.debug.file",
+{
+	#json <- system.file("testdata", "<json.fle.here>", package = "provDebugR")
+	#provDebugR::prov.debug.file(json)
+	
+	#expect_true(provDebugR:::.debug.env$has.graph)
+	
+	# TODO - expect test on rest of .debug.env variables
+})
 
 # prov.debug.run (general case)
+test_that("Initialization - prov.debug.run",
+{
+	#file <- system.file("testscripts", "<r.fle.here>", package = "provDebugR")
+	
+	# TODO
+	# check provenance generated matched one in expected folder
+	# expected json generated from create-json
+	
+	#expect_true(provDebugR:::.debug.env$has.graph)
+	
+	# TODO - make sure to delete prov folder that running the script generated
+})
 
 # prov.debug (general case)
+test_that("Initialization - prov.debug",
+{
+	#file <- system.file("testscripts", "<r.fle.here>", package = "provDebugR")
+	#rdtLite::prov.run(file)
+	
+	# TODO
+	# check provenance genreated matched one in expected folder
+	# expected json generated from create-json
+	
+	#provDebugR::prov.debug()
+	#expect_true(provDebugR:::.debug.env$has.graph)
+	
+	# TODO - make sure to delete prov folder that running the script generated
+})
 
 # empty prov
 test_that("Initialization - Empty provenance", 
@@ -17,7 +52,7 @@ test_that("Initialization - Empty provenance",
 })
 
 # .get.full.code
-test_that(".get.full.code",
+test_that("Initialization - .get.full.code",
 {
 	# uses test case for warnings and errors
 	# multiple scripts, some proc nodes span multiple lines
