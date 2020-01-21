a0ab74cd949c92f21903f641adaa1fc25b5de126
fong22e <fong22e@mtholyoke.edu>
2020-01-20 23:47:38 -0500

    fixed tests when all is TRUE

--- tests/testthat/test_lineage.R
+++ tests/testthat/test_lineage.R
@@ -104,49 +104,55 @@ get.expected.forward <- function()
 						   startLine = as.integer(c(2,3,11,12,15,17)),
 						   stringsAsFactors = FALSE)
 	
-	# index 4: e
+	# index 4: output
+	output <- data.frame(scriptNum = 1L,
+						 startLine = 3L,
+						 code = 'print(vector.1)',
+						 stringsAsFactors = FALSE)
+	
+	# index 5: e
 	# omit code column
 	e <- data.frame(scriptNum = rep(1L,7),
 					startLine = as.integer(c(5,7,8,11,12,16,17)),
 					stringsAsFactors = FALSE)
 	
-	# index 5: f
+	# index 6: f
 	# omit code column
 	f <- data.frame(scriptNum = rep(1L,7),
 					startLine = as.integer(c(6,7,8,11,12,16,17)),
 					stringsAsFactors = FALSE)
 	
-	# index 6: vector.2
+	# index 7: vector.2
 	# omit code column
 	vector.2 <- data.frame(scriptNum = rep(1L,6),
 						   startLine = as.integer(c(7,8,11,12,16,17)),
 						   stringsAsFactors = FALSE)
 	
-	# index 7: dev.2
+	# index 8: dev.2
 	# omit code column
 	dev.2 <- data.frame(scriptNum = rep(1L,3),
 						startLine = as.integer(c(11,12,17)),
 						stringsAsFactors = FALSE)
 	
-	# index 8: vector.3
+	# index 9: vector.3
 	# omit code column
 	vector.3 <- data.frame(scriptNum = rep(1L,2),
 						   startLine = as.integer(c(15,17)),
 						   stringsAsFactors = FALSE)
 	
-	# index 9: vector.4
+	# index 10: vector.4
 	# omit code column
 	vector.4 <- data.frame(scriptNum = rep(1L,2),
 						   startLine = as.integer(c(16,17)),
 						   stringsAsFactors = FALSE)
 	
-	# index 10: b
+	# index 11: b
 	b <- data.frame(scriptNum = as.integer(c(1,1)),
 					startLine = as.integer(c(20,21)),
 					code = c('g <- b + 50', 'print(g)'),
 					stringsAsFactors = FALSE)
 	
-	# index 11: g
+	# index 12: g
 	# identical to b
 	g <- data.frame(scriptNum = as.integer(c(1,1)),
 					startLine = as.integer(c(20,21)),
@@ -154,10 +160,10 @@ get.expected.forward <- function()
 					stringsAsFactors = FALSE)
 	
 	# combine into list
-	expected <- list(d, a, vector.1, e, f, vector.2, dev.2, 
-					 vector.3, vector.4, b, g)
-	names(expected) <- c("d", "a", "vector.1", "e", "f", "vector.2", "dev.2",
-						 "vector.3", "vector.4", "b", "g")
+	expected <- list(d, a, vector.1, output, e, f, vector.2, 
+					 dev.2, vector.3, vector.4, b, g)
+	names(expected) <- c("d", "a", "vector.1", "output", "e", "f", "vector.2", 
+						 "dev.2", "vector.3", "vector.4", "b", "g")
 	
 	return(expected)
 }
@@ -266,7 +272,7 @@ test_that("debug.lineage - all (backward)",
 test_that("debug.lineage - all (forward)",
 {
 	# all
-	c2 <- utils::capture.output(c1 <- debug.lineage(all = TRUE, forward = TRUE))
+	c1 <- debug.lineage(all = TRUE, forward = TRUE)
 	
 	c1$a <- c1$a[ , -3]   # omit code columns from testing
 	c1$d <- c1$d[ , -3]
@@ -279,79 +285,74 @@ test_that("debug.lineage - all (forward)",
 	c1$`vector.4` <- c1$`vector.4`[ , -3]
 	
 	expect_equivalent(c1, e.forward)
-	expect_true(nchar(paste(c2, collapse='\n')) > 0)
 	
 	# with a name queries (no lineage, valid, invalid)
-	c4 <- utils::capture.output(
-		c3 <- debug.lineage("output", "a", "invalid", all = TRUE, forward = TRUE))
+	c2 <- debug.lineage("output", "a", "invalid", all = TRUE, forward = TRUE)
 	
-	c3$a <- c3$a[ , -3]   # omit code columns from testing
-	c3$d <- c3$d[ , -3]
-	c3$e <- c3$e[ , -3]
-	c3$f <- c3$f[ , -3]
-	c3$`dev.2` <- c3$`dev.2`[ , -3]
-	c3$`vector.1` <- c3$`vector.1`[ , -3]
-	c3$`vector.2` <- c3$`vector.2`[ , -3]
-	c3$`vector.3` <- c3$`vector.3`[ , -3]
-	c3$`vector.4` <- c3$`vector.4`[ , -3]
+	c2$a <- c2$a[ , -3]   # omit code columns from testing
+	c2$d <- c2$d[ , -3]
+	c2$e <- c2$e[ , -3]
+	c2$f <- c2$f[ , -3]
+	c2$`dev.2` <- c2$`dev.2`[ , -3]
+	c2$`vector.1` <- c2$`vector.1`[ , -3]
+	c2$`vector.2` <- c2$`vector.2`[ , -3]
+	c2$`vector.3` <- c2$`vector.3`[ , -3]
+	c2$`vector.4` <- c2$`vector.4`[ , -3]
 	
-	expect_equivalent(c3, e.forward)
-	expect_true(nchar(paste(c4, collapse='\n')) > 0)
+	expect_equivalent(c2, e.forward)
 	
 	# start line query (valid)
-	c5 <- debug.lineage(start.line = 16, all = TRUE, forward = TRUE)[[1]]
-	c5 <- c5[ , -3]   # omit code column from testing
+	c3 <- debug.lineage(start.line = 16, all = TRUE, forward = TRUE)[[1]]
+	c3 <- c3[ , -3]   # omit code column from testing
 	
-	e5 <- e.forward$`vector.4`
+	e3 <- e.forward$`vector.4`
 	
-	expect_equivalent(c5, e5)
+	expect_equivalent(c3, e3)
 	
 	# start line query (invalid)
-	c7 <- utils::capture.output(
-		c6 <- debug.lineage(start.line = 100, all = TRUE, forward = TRUE))
+	c5 <- utils::capture.output(
+		c4 <- debug.lineage(start.line = 100, all = TRUE, forward = TRUE))
+	
+	expect_null(c4)
+	expect_true(nchar(paste(c5, collapse='\n')) > 0)
+	
+	# start line query (multiple)
+	expect_warning(
+		c7 <- utils::capture.output(
+			c6 <- debug.lineage(start.line = c(1:2), all = TRUE, forward = TRUE)))
 	
 	expect_null(c6)
 	expect_true(nchar(paste(c7, collapse='\n')) > 0)
 	
-	# start line query (multiple)
-	expect_warning(
-		c9 <- utils::capture.output(
-			c8 <- debug.lineage(start.line = c(1:2), all = TRUE, forward = TRUE)))
-	
-	expect_null(c8)
-	expect_true(nchar(paste(c9, collapse='\n')) > 0)
-	
 	# script num query (valid)
-	c11 <- utils::capture.output(
-		c10 <- debug.lineage(script.num = 1, all = TRUE, forward = TRUE))
+	c8 <- debug.lineage(script.num = 1, all = TRUE, forward = TRUE)
 	
-	c10$a <- c10$a[ , -3]   # omit code columns from testing
-	c10$d <- c10$d[ , -3]
-	c10$e <- c10$e[ , -3]
-	c10$f <- c10$f[ , -3]
-	c10$`dev.2` <- c10$`dev.2`[ , -3]
-	c10$`vector.1` <- c10$`vector.1`[ , -3]
-	c10$`vector.2` <- c10$`vector.2`[ , -3]
-	c10$`vector.3` <- c10$`vector.3`[ , -3]
-	c10$`vector.4` <- c10$`vector.4`[ , -3]
+	c8$a <- c8$a[ , -3]   # omit code columns from testing
+	c8$d <- c8$d[ , -3]
+	c8$e <- c8$e[ , -3]
+	c8$f <- c8$f[ , -3]
+	c8$`dev.2` <- c8$`dev.2`[ , -3]
+	c8$`vector.1` <- c8$`vector.1`[ , -3]
+	c8$`vector.2` <- c8$`vector.2`[ , -3]
+	c8$`vector.3` <- c8$`vector.3`[ , -3]
+	c8$`vector.4` <- c8$`vector.4`[ , -3]
 	
-	expect_equivalent(c10, e.forward)
-	expect_true(nchar(paste(c11, collapse='\n')) > 0)
+	expect_equivalent(c8, e.forward)
 	
 	# script num query (invalid)
-	c13 <- utils::capture.output(
-		c12 <- debug.lineage(script.num = 100, all = TRUE, forward = TRUE))
+	c10 <- utils::capture.output(
+		c9 <- debug.lineage(script.num = 100, all = TRUE, forward = TRUE))
 	
-	expect_null(c12)
-	expect_true(nchar(paste(c13, collapse='\n')) > 0)
+	expect_null(c9)
+	expect_true(nchar(paste(c10, collapse='\n')) > 0)
 	
 	# script num query (multiple)
 	expect_warning(
-		c15 <- utils::capture.output(
-			c14 <- debug.lineage(script.num = c(1:2), all = TRUE, forward = TRUE)))
+		c12 <- utils::capture.output(
+			c11 <- debug.lineage(script.num = c(1:2), all = TRUE, forward = TRUE)))
 	
-	expect_null(c14)
-	expect_true(nchar(paste(c15, collapse='\n')) > 0)
+	expect_null(c11)
+	expect_true(nchar(paste(c12, collapse='\n')) > 0)
 })
 
 # backwards lineage - name queries (valid and invalid)
