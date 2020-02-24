f70ff70e4187a24ca9bdd12efce9ececc96879fd
fong22e <fong22e@mtholyoke.edu>
2020-02-24 05:25:55 -0500

    .get.full.code.helper tests debugged

--- tests/testthat/test_init.R
+++ tests/testthat/test_init.R
@@ -23,29 +23,31 @@ test_that("Initialization - .get.full.code",
 	proc.nodes <- provParseR::get.proc.nodes(prov)
 	
 	# saved scripts
-	script1 <- system.file("testscripts", "exceptions.R", package = provDebugR)
-	script2 <- system.file("testscripts", "source_warning.r", package = provDebugR)
-	script3 <- system.file("testscripts", "source_error.r", package = provDebugR)
+	script1 <- system.file("testscripts", "exceptions.R", package = "provDebugR")
+	script2 <- system.file("testscripts", "source_warning.r", package = "provDebugR")
+	script3 <- system.file("testscripts", "source_error.r", package = "provDebugR")
 	
-	s1 <- data.frame(script = c(script1, script2, script3),            # all scripts are found
-					 stringsAsFactors = FALSE)
-	s2 <- data.frame(script = c("unknown1", "unknown2", "unknown3"),   # no scripts found
-					 stringsAsFactors = FALSE)
-	s3 <- data.frame(script = c(script1, script3, "unknown3"),         # some scripts found, some not found
-					 stringsAsFactors = FALSE)
+	s1 <- c(script1, script2, script3)            # all scripts are found
+	s2 <- c("unknown1", "unknown2", "unknown3")   # no scripts found
+	s3 <- c(script1, script2, "unknown3")         # some scripts found, some not found
 	
 	# CASES
-	c1 <- provDebugR:::.get.full.code.helper(proc.nodes, s1)   # all scripts are found
-	c2 <- provDebugR:::.get.full.code.helper(proc.nodes, s2)   # no scripts found
-	c3 <- provDebugR:::.get.full.code.helper(proc.nodes, s3)   # some scripts found, some not found
+	c1 <- provDebugR:::.get.full.code.helper(proc.nodes, s1)        # all scripts are found
+	expect_warning(                                                 # no scripts found
+		c2 <- provDebugR:::.get.full.code.helper(proc.nodes, s2))
+	expect_warning(                                                 # some scripts found, some not found
+		c3 <- provDebugR:::.get.full.code.helper(proc.nodes, s3))
 	
 	# EXPECTED
 	e1 <- system.file("testexpected", "fullCode.csv", package = "provDebugR")
 	e3 <- system.file("testexpected", "fullCode_missingScripts.csv", package = "provDebugR")
 	
-	e1 <- read.csv(e1, header = TRUE, row.names = 1, stringsAsFactors = FALSE)   # all scripts are found
-	e2 <- proc.nodes$name                                                        # no scripts found
-	e3 <- read.csv(e3, header = TRUE, row.names = 1, stringsAsFactors = FALSE)   # some scripts found, some not found
+	e1 <- read.csv(e1, header = TRUE, row.names = 1, stringsAsFactors = FALSE)
+	e3 <- read.csv(e3, header = TRUE, row.names = 1, stringsAsFactors = FALSE)
+	
+	e1 <- e1[ , 1]          # all scripts are found
+	e2 <- proc.nodes$name   # no scripts found
+	e3 <- e3[ , 1]          # some scripts found, some not found
 	
 	# TEST
 	expect_equal(c1, e1)
