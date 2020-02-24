74822729a5692d40f0ccd5c2d751343aeb153b13
fong22e <fong22e@mtholyoke.edu>
2020-02-24 04:52:50 -0500

    .get.full.code tests

--- tests/testthat/test_init.R
+++ tests/testthat/test_init.R
@@ -3,47 +3,6 @@ library(provDebugR)
 
 context("Initialization")
 
-# prov.debug.file (general case)
-test_that("Initialization - prov.debug.file",
-{
-	#json <- system.file("testdata", "<json.fle.here>", package = "provDebugR")
-	#prov.debug.file(json)
-	
-	#expect_true(provDebugR:::.debug.env$has.graph)
-	
-	# TODO - expect test on rest of .debug.env variables
-})
-
-# prov.debug.run (general case)
-test_that("Initialization - prov.debug.run",
-{
-	#file <- system.file("testscripts", "<r.fle.here>", package = "provDebugR")
-	
-	# TODO
-	# check provenance generated matched one in expected folder
-	# expected json generated from create-json
-	
-	#expect_true(provDebugR:::.debug.env$has.graph)
-	
-	# TODO - make sure to delete prov folder that running the script generated
-})
-
-# prov.debug (general case)
-test_that("Initialization - prov.debug",
-{
-	#file <- system.file("testscripts", "<r.fle.here>", package = "provDebugR")
-	#rdtLite::prov.run(file)
-	
-	# TODO
-	# check provenance genreated matched one in expected folder
-	# expected json generated from create-json
-	
-	#prov.debug()
-	#expect_true(provDebugR:::.debug.env$has.graph)
-	
-	# TODO - make sure to delete prov folder that running the script generated
-})
-
 # empty prov
 test_that("Initialization - Empty provenance", 
 {
@@ -53,50 +12,43 @@ test_that("Initialization - Empty provenance",
 })
 
 # .get.full.code
-# .get.full.code throws a warning when files are not found.
-# this makes the initialisation functions throw the warning too.
+# tests the helper function of .get.full.code
+# this is to prevent travis from having to run rdt/rdtLite
 test_that("Initialization - .get.full.code",
 {
-	# GENERAL CASE
-	# uses test case for warnings and errors
-	# multiple scripts, some proc nodes span multiple lines
+	# FUNCTION INPUTS
+	# get proc.nodes
 	json <- system.file("testdata", "exceptions.json", package = "provDebugR")
+	prov <- provParseR::prov.parse(json, isFile = TRUE)
+	proc.nodes <- provParseR::get.proc.nodes(prov)
 	
-	prov.debug.file(json)
-	c1 <- provDebugR:::.get.full.code()
-	c1 <- data.frame(c1, stringsAsFactors = FALSE)
+	# saved scripts
+	script1 <- system.file("testscripts", "exceptions.R", package = provDebugR)
+	script2 <- system.file("testscripts", "source_warning.r", package = provDebugR)
+	script3 <- system.file("testscripts", "source_error.r", package = provDebugR)
 	
-	e1 <- system.file("testexpected", "fullCode_exceptions.csv", package = "provDebugR")
-	e1 <- read.csv(e1, header = TRUE, row.names = 1, stringsAsFactors = FALSE)
+	s1 <- data.frame(script = c(script1, script2, script3),            # all scripts are found
+					 stringsAsFactors = FALSE)
+	s2 <- data.frame(script = c("unknown1", "unknown2", "unknown3"),   # no scripts found
+					 stringsAsFactors = FALSE)
+	s3 <- data.frame(script = c(script1, script3, "unknown3"),         # some scripts found, some not found
+					 stringsAsFactors = FALSE)
 	
-	expect_equivalent(c1, e1)
+	# CASES
+	c1 <- provDebugR:::.get.full.code.helper(proc.nodes, s1)   # all scripts are found
+	c2 <- provDebugR:::.get.full.code.helper(proc.nodes, s2)   # no scripts found
+	c3 <- provDebugR:::.get.full.code.helper(proc.nodes, s3)   # some scripts found, some not found
 	
-	# EDGE CASE 1
-	# case where no script files could be found
-	# this uses the test case for type changes
-	provDebugR:::.clear()
-	json <- system.file("testdata", "typeChanges.json", package = "provDebugR")
-	
-	expect_warning(prov.debug.file(json))
-	expect_warning(c2 <- provDebugR:::.get.full.code())
-	c2 <- data.frame(c2, stringsAsFactors = FALSE)
-	
-	e2 <- system.file("testexpected", "fullCode_typeChanges.csv", package = "provDebugR")
-	e2 <- read.csv(e2, header = TRUE, row.names = 1, stringsAsFactors = FALSE)
-	
-	expect_equivalent(c2, e2)
-	
-	# EDGE CASE 2
-	# case where only some of the script files could be found
-	provDebugR:::.clear()
-	json <- system.file("testdata", "missingScripts.json", package = "provDebugR")
-	
-	expect_warning(prov.debug.file(json))
-	expect_warning(c3 <- provDebugR:::.get.full.code())
-	c3 <- data.frame(c3, stringsAsFactors = FALSE)
-	
+	# EXPECTED
+	e1 <- system.file("testexpected", "fullCode.csv", package = "provDebugR")
 	e3 <- system.file("testexpected", "fullCode_missingScripts.csv", package = "provDebugR")
-	e3 <- read.csv(e3, header = TRUE, row.names = 1, stringsAsFactors = FALSE)
 	
-	expect_equivalent(c3, e3)
+	e1 <- read.csv(e1, header = TRUE, row.names = 1, stringsAsFactors = FALSE)   # all scripts are found
+	e2 <- proc.nodes$name                                                        # no scripts found
+	e3 <- read.csv(e3, header = TRUE, row.names = 1, stringsAsFactors = FALSE)   # some scripts found, some not found
+	
+	# TEST
+	expect_equal(c1, e1)
+	expect_equal(c2, e2)
+	expect_equal(c3, e3)
 })
