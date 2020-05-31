836013f6c90fd74e9f792c36dc27eaa6973064f7
fong22e <fong22e@mtholyoke.edu>
2020-05-31 10:12:46 -0400

    modified fromEnv test

--- inst/testscripts/source_fromEnv.R
+++ inst/testscripts/source_fromEnv.R
@@ -8,13 +8,18 @@ vector.2 <- c(e:f)
 print(vector.2)
 
 # create a plot
+X11()
 plot(vector.1, vector.2)
 title("Plot 1")
+dev.off()
 
-# create another plot, end without using dev.off
+# create another plot, end using dev.off
+pdf("plot.pdf")
 vector.3 <- vector.1 + 20L
 vector.4 <- vector.2 + 30L
 plot(vector.3, vector.4)
+title("pdf plot")
+dev.off()
 
 # use b so that there's a forward lineage that does not end in a plot
 g <- b + 50
